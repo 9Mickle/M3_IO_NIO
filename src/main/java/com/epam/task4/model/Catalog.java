ff15932d19fd7641110b62adadace51c6042c2f2
Mikhail Zaks <mikhail_zaks@epam.com>
2022-01-17 18:27:58 +0300

    refactor Catalog

--- src/main/java/com/epam/task4/model/Catalog.java
+++ src/main/java/com/epam/task4/model/Catalog.java
@@ -1,5 +1,9 @@
 package com.epam.task4.model;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.List;
 
 @XmlRootElement(name = "catalog")
